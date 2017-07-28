2c3525fff757fc51a224f35a8d8bbb0b3b0926a2
JDC-User <JDC-User@DESKTOP-P5GA6CT>
2017-07-28 14:40:50 +0630

    sale & rent

---
+++ home/src/main/java/com/jdc/home/beans/SaleRentBean.java
@@ -0,0 +1,13 @@
+package com.jdc.home.beans;
+
+import java.io.Serializable;
+
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+
+@SuppressWarnings("serial")
+@Named
+@FlowScoped("salerent")
+public class SaleRentBean implements Serializable {
+
+}
